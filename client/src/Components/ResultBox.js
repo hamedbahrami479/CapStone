5fd374ff4d374e61bad78b25d0056de2cf73ea73
hamedbahrami479 <bahrami.hamed479@googlemail.com>
2022-05-11 15:04:41 +0200

    Zwischenspeicherung

--- client/src/Components/ResultBox.js
+++ client/src/Components/ResultBox.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import productHandling from './Daten';
 
+function ResultBox({ productHandling }) {
+  const [product, setProduct] = useState(false);
+  return <InputContainer type="text" id="result" name="result">
 
-const ResultBox= () => {
-  return <input className="resultBox" type="text" id="result" name="result"></input>;
-};
+    
+  </InputContainer>;
+}
 
 export default ResultBox;
+
+const InputContainer = styled.input`
+  size: 10ch;
+  background-color: rgb(0, 225, 255);
+  border-radius: 2rem;
+  height: 10rem;
+  width: 80%;
+  margin: auto;
+  display: block;
+`;
--- client/src/Components/button.js
+++ client/src/Components/button.js
@@ -2,24 +2,21 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 
-/*function searchCorrectHandling() {
-  const [product, setProduct] = useState()
-
-function getCorrectHandling () {
-  const correctHandling = useEffect(() => {
-    localStorage.getItem('product')
-  })
-}
-on Click={searchCorrectHandling}
-*/
-
-
-
 const SearchButton = () => {
   return (
     <div className="container">
-      <button>Search</button>
+      <ButtonStyled>Search</ButtonStyled>
     </div>
   );
 };
+
+const ButtonStyled = styled.button`
+  padding: 0.7rem 2rem;
+  border-radius: 0.5rem;
+  background-color: var(--color-red);
+  border: none;
+  color: white;
+  font-style: italic;
+  font-weight: 900;`
+
 export default SearchButton;
